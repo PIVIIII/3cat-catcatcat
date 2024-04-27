77af4f7bcde788d7cf9bdf022be66f35e5999f28
Nitchakan <nitchakan.kkng@gmail.com>
2024-04-27 14:50:49 +0700

    update View all plan button

--- src/components/SpecialPrivileges.tsx
+++ src/components/SpecialPrivileges.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useRouter } from "next/navigation"
+import { useEffect } from 'react';
+
 
 export default function SpecialPrivileges() {
 
@@ -69,10 +71,6 @@ export default function SpecialPrivileges() {
             <div className="pt-10 pb-10"></div>
 
             <img src="/img/premium_2.png" alt="cute cat" className="inset-x-0 bottom-0" />
-
-
-
-
 
         </div>
     )
